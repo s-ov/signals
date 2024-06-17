c5e4bafd1e451246d1cc2abf0e0e0b35d9ca0b54
s-ov <s.ovsiuk@gmail.com>
2024-06-17 23:14:28 +0300

    Initial commit

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    phone = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return str(self.user)
+    
